46970f39e9c39deb5e4e76fbb2f2c26199972462
Abu-Judeh <muhammad-herbawi@hotmail.com>
2023-04-24 00:03:33 +0200

    3_report

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
 ﻿using Calculator_Kata;
 
-Product book = new Product("The Little Prince", 12345, (decimal)20.25);
-const decimal tax=20;
-const decimal discount = 15;
-PriceCalculator Calc = new PriceCalculator(tax, discount);
+Product book = new Product("The Little Prince", 12345, 20.25m);
 
-Console.WriteLine("Product: " + book.Name);
-Console.WriteLine("UPC: " + book.UPC);
-Console.WriteLine("Price before tax and discount: " + book.Price.ToString("C"));
-Console.WriteLine("Final price: " + Calc.CalculateFinalPrice(book.Price));
-Console.WriteLine("Tax Amount: " + Calc.CalculateTax(book.Price));
-Console.WriteLine("discount Amount: " + Calc.CalculateDiscount(book.Price));
+Console.WriteLine($"Product: {book.Name}");
+Console.WriteLine($"UPC: {book.UPC}");
+Console.WriteLine($"Price before tax and discount: ${book.Price}");
+
+decimal taxPercentage = 20;
+decimal discountPercentage = 0;
+
+Console.WriteLine($"Price after {taxPercentage}% tax: ${book.GetPriceWithTax(taxPercentage)}");
+
+decimal finalPrice = book.GetPriceWithTaxAndDiscount(taxPercentage, discountPercentage);
+Console.WriteLine($"Price after {taxPercentage}% tax and {discountPercentage}% discount: ${finalPrice}");
+
+if (discountPercentage > 0)
+{
+    decimal discountAmount = Math.Round(book.Price * (discountPercentage / 100), 2);
+    Console.WriteLine($"Discounted amount: ${discountAmount}");
+}
+
+Console.ReadLine();
