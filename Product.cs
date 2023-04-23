bcbece8a08ec553bed58d24954f72f48f31e54a7
Abu-Judeh <muhammad-herbawi@hotmail.com>
2023-04-24 00:03:33 +0200

    add methods to calculate price with tax and price with tax and discount in product class print all the data needed to be print in  program class

--- Product.cs
+++ Product.cs
@@ -5,11 +5,24 @@ public class Product
     public string Name { get; set; }
     public int UPC { get; set; }
     public decimal Price { get; set; }
+
     public Product(string name, int upc, decimal price)
     {
         Name = name;
         UPC = upc;
         Price = price;
+    }
+
+    public decimal GetPriceWithTax(decimal taxPercentage)
+    {
+        return Math.Round(Price * (1 + taxPercentage / 100), 2);
+    }
+
+    public decimal GetPriceWithTaxAndDiscount(decimal taxPercentage, decimal discountPercentage)
+    {
+        decimal priceWithTax = GetPriceWithTax(taxPercentage);
+        decimal discountAmount = Math.Round(Price * (discountPercentage / 100), 2);
+        return Math.Round(priceWithTax - discountAmount, 2);
     }
 
     
